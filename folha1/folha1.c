05e5306af4597737922d29689e72b5e2765a7cc3
hiddenduck <74628557+hiddenduck@users.noreply.github.com>
2021-02-23 12:28:15 +0000

    Add files via upload

---
+++ folha1/folha1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Ficha 1
+//1
+//1.1
+//Output: 12 16
+
+//3
+//3.1
+void quadrado (int n){
+    if (n == 0){
+        return ();
+    }else{
+        putchar('#');
+        return()
+    }
+}
+
+
+void quadrado (int n){
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            putchar('#');
+        }
+        putchar('\n');
+    }  
+}
+//3.2
+void quadrado2 (int n){
+    for (int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(((i + j) % 2)== 0)
+                putchar('#');
+            else
+                putchar('_');
+        }
+        putchar('\n');
+    }  
+}
+
+//3.3
+void vertical (i)
+
+void main(){
+    triangulo(5);
+    
+}
+//3.3

cf7baaba9fc16b2fb02045f89538f5ad94a674ea
hiddenduck <74628557+hiddenduck@users.noreply.github.com>
2021-03-03 12:40:51 +0000

    Update folha1.c

--- folha1/folha1.c
+++ folha1/folha1.c
@@ -106,4 +106,21 @@ void quadrado2 (int n){
         putchar('\n');
     }  
 }
-
+//4
+int verifica (int i, int j, int r){
+    if(pow((i-r),2) + pow((j-r),2) <= r*r)
+        return 1;
+    else
+        return 0;
+}
+void circulo (int r){
+    for(int i = 0; i <= 2*r; i++){
+        for(int j = 0; j <= 2*r; j++){
+            if(verifica(i,j,r) == 1)
+                putchar('#');
+            else
+                putchar(' ');
+        }
+        putchar('\n');
+    }
+}

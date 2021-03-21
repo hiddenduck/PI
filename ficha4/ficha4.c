849ef1ee246ab893971a4c663c08c78f47c3d924
hiddenduck <cmachado0032@gmail.com>
2021-03-21 13:15:51 +0000

    Update ficha4.c

--- ficha4/ficha4.c
+++ ficha4/ficha4.c
@@ -2,25 +2,8 @@
 #include <ctype.h>
 
 //1
-int minusculas (char s[]){
-    int contador = 0;
-    for(int i=0; i<strlen(s); i++){
-        if (s[i] >= 65 && s[i] <= 90){
-            s[i] += 32;
-            contador++;
-        }
-    }
-    return contador;
-}
-
-//2
-int contalinhas (char s[]){
-    int contador = 0;
-    for(int i=0; i<strlen(s); i++){
-        if(s[i] == 10)
-            contador++;
-    }
-    return contador;
+int contaVogais (char *s){
+    int contador = 0;    
 }
 
 
